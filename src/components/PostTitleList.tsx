3e70c6175598aa5a4932da0a6459872454d5647b
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-09-29 10:40:32 -0400

    fetching post data

--- src/components/PostTitleList.tsx
+++ src/components/PostTitleList.tsx
@@ -1,15 +1,33 @@
 import Link from "next/link";
 import { Suspense } from "react";
+import mysql from "mysql2";
 
 interface post {
   title: string;
   id: string;
 }
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "patientdb",
+});
+const runQuery = () => {
+  // simple query
+  connection.query(
+    "SELECT id, title FROM posts",
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
+};
+
 //Call api with lambda function, that runs SQL
 const getPosts = async (): Promise<post[]> => {
   //const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   //const posts: post[] = await response.json();
+  runQuery();
   const mockPostTitles: post[] = [
     { title: "test", id: "1" },
     { title: "test2", id: "2" },
