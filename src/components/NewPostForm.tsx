136510b5dae89bd0f3000fafe1fcfae597a2f174
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-09-29 12:00:36 -0400

    fix post submit console log

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,17 +1,20 @@
 "use client";
-import { useState } from "react";
+
+import { FormEvent, useState } from "react";
 
 const NewPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     console.log({ title, content });
+
     //POST to lambda function that will write to DB (with author ID data)
     //In somewhere validate if user can post
   };
   return (
-    <form className="flex flex-col gap-4">
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
       <div className="flex flex-col">
         <label>Title</label>
         <input
@@ -26,11 +29,11 @@ const NewPostForm = () => {
         <textarea
           value={content}
           onChange={(e) => setContent(e.target.value)}
+          className="text-black"
         ></textarea>
       </div>
       <button
         type="submit"
-        onSubmit={handleSubmit}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Add Post
